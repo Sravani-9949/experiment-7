bd542a9b1691daed18ea3ba943838d07617e5bc9
Sravani-9949 <sravanipatel9949@gmail.com>
2025-10-10 13:21:37 +0900

    Update samplename.java

--- samplename.java
+++ samplename.java
@@ -3,6 +3,6 @@ public class samplename
     public static void main(String args[])
     {
         String name = "Sravani";
-        System.out.println("Hello,My name is + name");
+        System.out.println("Hello,My name is" + name);
     }
 }
